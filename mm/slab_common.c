db6bef8354e60250b0e83c4fc9aff6b0c899ee8c
Daniel Micay <danielmicay@gmail.com>
2017-05-28 18:51:30 -0400

    mark kmem_cache as __ro_after_init
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -28,7 +28,7 @@
 enum slab_state slab_state __ro_after_init;
 LIST_HEAD(slab_caches);
 DEFINE_MUTEX(slab_mutex);
-struct kmem_cache *kmem_cache;
+struct kmem_cache *kmem_cache __ro_after_init;
 
 /*
  * Set of flags that will prevent slab merging
