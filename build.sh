b25a22ea0a9d09c9d653d5f7bf11da51096d51f1
Daniel Micay <danielmicay@gmail.com>
2019-03-31 16:30:19 -0400

    add build script
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+[[ $# -eq 1 ]] || exit 1
+
+DEVICE=$1
+
+if [[ $DEVICE != blueline && $DEVICE != crosshatch && $DEVICE != bonito ]]; then
+    echo invalid device codename
+    exit 1
+fi
+
+ROOT_DIR=$(realpath ../../..)
+
+PATH="$ROOT_DIR/prebuilts/build-tools/linux-x86/bin:$PATH"
+PATH="$ROOT_DIR/prebuilts/build-tools/path/linux-x86:$PATH"
+PATH="$ROOT_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
+PATH="$ROOT_DIR/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH"
+PATH="$ROOT_DIR/prebuilts/clang/host/linux-x86/clang-r383902/bin:$PATH"
+PATH="$ROOT_DIR/prebuilts/misc/linux-x86/lz4:$PATH"
+PATH="$ROOT_DIR/prebuilts/misc/linux-x86/dtc:$PATH"
+PATH="$ROOT_DIR/prebuilts/misc/linux-x86/libufdt:$PATH"
+export LD_LIBRARY_PATH="$ROOT_DIR/prebuilts/clang/host/linux-x86/clang-r383902/lib64:$LD_LIBRARY_PATH"
+
+export KBUILD_BUILD_VERSION=1
+export KBUILD_BUILD_USER=grapheneos
+export KBUILD_BUILD_HOST=grapheneos
+export KBUILD_BUILD_TIMESTAMP="$(date -ud "@$(git show -s --format=%ct)")"
+
+chrt -bp 0 $$
+
+make \
+    O=out \
+    ARCH=arm64 \
+    CC=clang \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    READELF=llvm-readelf \
+    OBJSIZE=llvm-size \
+    STRIP=llvm-strip \
+    HOSTCC=clang \
+    HOSTCXX=clang++ \
+    HOSTLD=ld.lld \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    ${DEVICE}_defconfig
+
+make -j$(nproc) \
+    O=out \
+    ARCH=arm64 \
+    CC=clang \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    READELF=llvm-readelf \
+    OBJSIZE=llvm-size \
+    STRIP=llvm-strip \
+    OBJCOPY=llvm-objcopy \
+    HOSTCC=clang \
+    HOSTCXX=clang++ \
+    HOSTLD=ld.lld \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+mkdir -p "$ROOT_DIR/device/google/$DEVICE-kernel"
+cp out/arch/arm64/boot/{dtbo.img,Image.lz4} "$ROOT_DIR/device/google/$DEVICE-kernel"
+
+if [[ $DEVICE != bonito ]]; then
+    cp out/arch/arm64/boot/dts/qcom/sdm845-v2.1.dtb "$ROOT_DIR/device/google/$DEVICE-kernel"
+    cp out/arch/arm64/boot/dts/qcom/sdm845-v2.dtb "$ROOT_DIR/device/google/$DEVICE-kernel"
+else
+    cp out/arch/arm64/boot/dts/qcom/sdm670.dtb "$ROOT_DIR/device/google/$DEVICE-kernel"
+fi
